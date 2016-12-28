1fe0afe24cd7fa53870c830bf685a60b0f2e9938
Caij <271945881@qq.com>
2016-12-28 11:24:20 +0800

    fiix login bug

--- app/src/main/java/com/caij/emore/present/imp/WeiCoLoginPresentImp.java
+++ app/src/main/java/com/caij/emore/present/imp/WeiCoLoginPresentImp.java
@@ -34,40 +34,40 @@ public class WeiCoLoginPresentImp extends AbsBasePresent implements LoginPresent
     public void getAccessToken(String clientId, String clientSecret, String grantType, final String redirectUrL, final String username, final String pwd) {
         mLoginView.showDialogLoading(true, R.string.logining);
         Subscription loginSubscription = mLoginApi.getAccessToken(clientId, clientSecret, grantType, redirectUrL)
-                .doOnNext(new Action1<Token>() {
-                    @Override
-                    public void call(Token accessToken) {
-                        UserPrefs userPrefs = UserPrefs.get(EMApplication.getInstance());
-                        Account account = new Account();
-                        long uid = Long.parseLong(accessToken.getUid());
-                        account.setUsername(username);
-                        account.setPwd(pwd);
-                        account.setStatus(Account.STATUS_USING);
-                        account.setUid(uid);
+            .doOnNext(new Action1<Token>() {
+                @Override
+                public void call(Token accessToken) {
+                    UserPrefs userPrefs = UserPrefs.get(EMApplication.getInstance());
+                    Account account = new Account();
+                    long uid = Long.parseLong(accessToken.getUid());
+                    account.setUsername(username);
+                    account.setPwd(pwd);
+                    account.setStatus(Account.STATUS_USING);
+                    account.setUid(uid);
 
-                        account.setToken(accessToken);
+                    account.setToken(accessToken);
 
-                        userPrefs.changeAccount(account);
-                    }
-                })
-                .compose(SchedulerTransformer.<Token>create())
-                .subscribe(new Subscriber<Token>() {
-                    @Override
-                    public void onCompleted() {
-                        mLoginView.showDialogLoading(false, R.string.logining);
-                    }
+                    userPrefs.changeAccount(account);
+                }
+            })
+            .compose(SchedulerTransformer.<Token>create())
+            .subscribe(new Subscriber<Token>() {
+                @Override
+                public void onCompleted() {
+                    mLoginView.showDialogLoading(false, R.string.logining);
+                }
 
-                    @Override
-                    public void onError(Throwable e) {
-                        mLoginView.showDialogLoading(false, R.string.logining);
-                        mLoginView.onDefaultLoadError();
-                    }
+                @Override
+                public void onError(Throwable e) {
+                    mLoginView.showDialogLoading(false, R.string.logining);
+                    mLoginView.onDefaultLoadError();
+                }
 
-                    @Override
-                    public void onNext(Token accessToken) {
-                        mLoginView.onLoginSuccess(accessToken);
-                    }
-                });
+                @Override
+                public void onNext(Token accessToken) {
+                    mLoginView.onLoginSuccess(accessToken);
+                }
+            });
         addSubscription(loginSubscription);
     }
 
